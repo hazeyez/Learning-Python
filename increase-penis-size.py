156ea4c90a771de67555a220840056ec33b8cded
hazeyez <90569746+hazeyez@users.noreply.github.com>
2022-10-01 01:56:31 -0400

    initial commit
    program that increases your penis size! playing with variables, functions and timers.

---
+++ increase-penis-size.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Author: hazeyez, copyright Dalnet default chan # ops.
+
+import time
+
+stub_size = float(input("Enter your penis size in inches: "))
+time.sleep(2)
+
+print("The size you entered is:", stub_size, "inches. Wow! That is a small penis!")
+time.sleep(2)
+
+print("Don't worry, together we're going to make it bigger...")
+time.sleep(2)
+
+growth = float(input("How many inches do you want it to grow? "))
+time.sleep(2)
+
+print("Okay! We're going to grow that penis by", growth, "inches!")
+time.sleep(1)
+
+print("Standby while our cocksmiths play with your prostate and put your penis into our proprietary sukKy sukKy vacuum!")
+
+for i in range(5,0,-1):
+    time.sleep(1)
+    print(i)
+
+def add_size(new_size):
+    grown_penis = stub_size + growth
+    return grown_penis
+
+new_penis = add_size(growth)
+time.sleep(1)
+
+print("Congradulations! Our expert cocksmiths were able to increase your penis size to:", new_penis, "inches.")
+
+print("Be careful with that thing! Also, as a reminder we are not responsible for shrinkage! You must masturbate 7x a day to maintain this new growth!")
